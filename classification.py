fbaab25dc3f4c9a7529af3e9d3aec4a8af9f2f7e
dickoma <dicko@MacBook-Pro-de-Mamadou.local>
2019-02-22 00:37:06 +0100

    MAJ

--- classification.py
+++ classification.py
@@ -1,6 +1,4 @@
-# -*- coding: utf-8 -*-
-import DATA_matrice_moyenne as DATA1
-import DATA_matrice_variance as DATA2
+import DATA_matrice_moyenne as DATA
 import load_DB as ldb
 import numpy as np
 
@@ -9,8 +7,7 @@ def classificationMoyenne(indice):
     mini = np.inf
     index = 0
     for i in range(10):
-        IntervalleConfiance = np.add(DATA1.matrice_moyenne,DATA2.matrice_variance)
-        D = np.subtract(M,IntervalleConfiance[i])
+        D = np.subtract(M,DATA.matrice_moyenne[i])
         d = np.linalg.norm(D)
        
         if d<mini:
@@ -18,22 +15,22 @@ def classificationMoyenne(indice):
             mini = d
     return index
 
-#
-#def classificationCosinus(indice):
-#    M = ldb.getData(indice)
-#    normeM =  np.linalg.norm(M)
-#    bestScore = 0 
-#    index = 0
-#    for i in range(10):
-#        mean = DATA.matrice_moyenne[i] 
-#        dotProduct = M.dot(mean)
-#        normeMean =  np.linalg.norm(mean)
-#        cosinus = dotProduct/(normeM*normeMean) # normeM et normeMean sont forcement differentes de 0
-#        if bestScore<cosinus:
-#            index = i
-#            bestScore = cosinus
-#            
-#    return index
+
+def classificationCosinus(indice):
+    M = ldb.getData(indice)
+    normeM =  np.linalg.norm(M)
+    bestScore = 0 
+    index = 0
+    for i in range(10):
+        mean = DATA.matrice_moyenne[i] 
+        dotProduct = M.dot(mean)
+        normeMean =  np.linalg.norm(mean)
+        cosinus = dotProduct/(normeM*normeMean) # normeM et normeMean sont forcement differentes de 0
+        if bestScore<cosinus:
+            index = i
+            bestScore = cosinus
+            
+    return index
 
 """
     Retourne la liste des K (K<=45) couples de chiffres les plus confondus ainsi que le nombre de fois qu'ils ont été confondus
@@ -66,6 +63,6 @@ def successRate(Test,algorithme):
 
 Training , Test = ldb.seperateData()
 
-print("Classification Moyenne :",successRate(Training,classificationMoyenne))
-#
-#print("Classification Cosinus :",successRate(Test,classificationCosinus))
+print("Classification Moyenne :",successRate(Test,classificationMoyenne))
+
+print("Classification Cosinus :",successRate(Test,classificationCosinus))
