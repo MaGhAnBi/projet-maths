27fb1185b0d69962911b7006d6457fb84d6fd1a2
Bijan <jhmj>
2019-03-30 23:30:48 +0100

    stockage base de donnée ok, distance not ok

--- GenerateTransformedData.py
+++ GenerateTransformedData.py
@@ -1,23 +1,34 @@
 import numpy as np
+from scipy.linalg import qr
+from scipy.io import savemat
 import load_DB as ldb
 import PIL
 import matplotlib.pyplot as plt
 
-"""
-    Fonction : Translate d'un pas de 'alpha' à toutes les images de la base de données database
-               en appliquant la translation 'translate'
-"""
 
-def translation (database,alpha,translate) :
+
+def translationDB(database,alpha,translate) :
+    """
+    Fonction : Translate de pas de -alpha à alpha en le sauvegardent le resultat dans translate.mat
+    """
+    dic = {}
+    nbData = (2*alpha+1)*sum(ldb.nbChiffre)
+    dic["data"] = np.zeros((nbData,784))
+    dic["label"] = np.zeros(nbData)
+    i=0
     for image in database:
-        translated = translate(ldb.getData(image),alpha)
+        dic["data"][i] = translate(ldb.getData(image),alpha)
+        dic["label"][i] = ldb.getLabel(image)
+        i+=1
+    savemat(translate.__name__,dic)
 
-"""
-    translateX : (image : 1x784, alphaX : int  ) ==> imageTranslated : 28x28
-    Fonction : Translate l'image 'image' de alphaX suivant l'Horizontal
-"""
+
 
 def translateX(image,alphaX) :
+    """
+    translateX : (image : 1x784, alphaX : int  ) ==> imageTranslated : 28x28
+    Fonction : Translate l'image 'image' de alphaX suivant l'Horizontal
+    """
     image = image.reshape((28,28))
     imageTranslated = np.zeros(image.shape, dtype="int32")
     if alphaX>= 0 :
@@ -27,13 +38,14 @@ def translateX(image,alphaX) :
         alphaX = -alphaX
         imageTranslated[:,alphaX:image.shape[1]] = image[:,0:(image.shape[1]-alphaX)] 
         imageTranslated = np.flip(imageTranslated,(0,1))
-    return imageTranslated
+    return imageTranslated.reshape(784)
 
-"""
+def translateY(image,alphaY) :
+    
+    """
     translateY : (image : 1x784, alphaX : int  ) ==> imageTranslated : 28x28
     Fonction : Translate l'image 'image' de alphaY suivant la verticale
-"""
-def translateY(image,alphaY) :
+    """
     image = image.reshape((28,28))
     imageTranslated = np.zeros(image.shape, dtype="int32")
     if alphaY>= 0 :
@@ -45,35 +57,42 @@ def translateY(image,alphaY) :
         imageTranslated = np.flip(imageTranslated,(0,1))
     return imageTranslated
     
-def TangenteClassificationDistance(dataBase,p,translation, p_translation_parameters, e_translation_parameters):
+def TangenteDistance(p,e,Tp,Te):
+    """
+    Fonction calcul la distance tangente entre deux images p,e
+    Entrées: p,e: deux images,  Tp: la matrice des transormations de p, Te: la matrice des transformations des e
+    Tp et Te ont la même dimension
+    Sortie: d: la distance tangente de p et e
+    """
+    A = np.zeros((Tp.shape[0],2*Tp.shape[1]))
+    A[:,0:Tp.shape[1]] = -1*Tp[:,:]
+    A[:,Tp.shape[1]:2*Tp.shape[1]] = Te[:,:] # A = (-Tp Te)
+    Q,R = qr(A) # decomposition QR de A
+    print(Q,R)
+    Q2 = Q[:,R.shape[0]-1:Q.shape[1]]
+    b = p-e
+    d = Q2.transpose()@b
+    return np.linalg.norm(d)
     
-    indice = 0
-    dmin = np.inf
-    for e in dataBase :
-        for alpha in p_translation_parameters:
-                tp = translation(p,alpha) - p.reshape((28,28))
-                for beta in e_translation_parameters:
-                    e_matrix = ldb.getData(dataBase)
-                    te = translation(e_matrix,beta) - e_matrix.reshape((28,28))
-                    d = np.linealg.norm(p+tp*alpha - e_matrix - (te*beta))
-                    if d < dmin : 
-                        d = dmin
-                        indice = e
+    
                         
-    
-trainingData,testData = ldb.seperateData()
-alpha = 10
-translation(trainingData,alpha,translateX)
-"""
-M = ldb.getData(10)
-trX = translateX(M,-5)
-trY = translateY(M,-0)
-#translation(trainingData,0)
-#img = translateX(M,1)
-plt.imshow(M.reshape(28,28))
-plt.show()
-plt.imshow(trX)
-plt.show()
-plt.imshow(trY)
-plt.show()
-"""
+p = ldb.getData(5)
+Tp = np.array([[2]])
+
+e = ldb.getData(15)
+Te = np.array([[-2]])
+
+Training, Test = ldb.seperateData()
+translationDB(Training,2,translateX)
+
+#M = ldb.getData(10)
+#trX = translateX(M,5)
+#trY = translateY(M,5)
+##translation(trainingData,0)
+##img = translateX(M,1)
+#plt.imshow(M.reshape(28,28))
+#plt.show()
+#plt.imshow(trX)
+#plt.show()
+#plt.imshow(trY)
+#plt.show()
