4521cf48ce801502fad05824662eede271d850c9
Bijan <jhmj>
2019-03-31 09:41:36 +0200

    derivation ajouté

--- GenerateTransformedData.py
+++ GenerateTransformedData.py
@@ -4,7 +4,7 @@ from scipy.io import savemat
 import load_DB as ldb
 import PIL
 import matplotlib.pyplot as plt
-
+from random import randint
 
 
 def translationDB(database,alpha,translate) :
@@ -14,10 +14,13 @@ def translationDB(database,alpha,translate) :
     dic = {}
     nbData = (2*alpha+1)*sum(ldb.nbChiffre)
     dic["data"] = np.zeros((nbData,784))
+    dic["derivation"] = np.zeros((nbData,784))
     dic["label"] = np.zeros(nbData)
     i=0
     for image in database:
-        dic["data"][i] = translate(ldb.getData(image),alpha)
+        img, T = translate(ldb.getData(image),alpha)
+        dic["data"][i] = img
+        dic["derivation"][i] = T
         dic["label"][i] = ldb.getLabel(image)
         i+=1
     savemat(translate.__name__,dic)
@@ -26,7 +29,7 @@ def translationDB(database,alpha,translate) :
 
 def translateX(image,alphaX) :
     """
-    translateX : (image : 1x784, alphaX : int  ) ==> imageTranslated : 28x28
+    translateX : (image : 1x784, alphaX : int  ) ==> imageTranslated, T:differentielle de l'operation
     Fonction : Translate l'image 'image' de alphaX suivant l'Horizontal
     """
     image = image.reshape((28,28))
@@ -37,13 +40,14 @@ def translateX(image,alphaX) :
         image = np.flip(image,(0,1))
         alphaX = -alphaX
         imageTranslated[:,alphaX:image.shape[1]] = image[:,0:(image.shape[1]-alphaX)] 
-        imageTranslated = np.flip(imageTranslated,(0,1))
-    return imageTranslated.reshape(784)
+        imageTranslated = np.flip(imageTranslated,(0,1)) 
+    T = (imageTranslated-image)/alphaX
+    return imageTranslated.reshape(784),T.reshape(784)
 
 def translateY(image,alphaY) :
     
     """
-    translateY : (image : 1x784, alphaX : int  ) ==> imageTranslated : 28x28
+    translateY : (image : 1x784, alphaX : int  ) ==> imageTranslated, T:differentielle de l'operation
     Fonction : Translate l'image 'image' de alphaY suivant la verticale
     """
     image = image.reshape((28,28))
@@ -55,7 +59,8 @@ def translateY(image,alphaY) :
         alphaY = -alphaY
         imageTranslated[alphaY:image.shape[1],:] = image[0:(image.shape[1]-alphaY),:] 
         imageTranslated = np.flip(imageTranslated,(0,1))
-    return imageTranslated
+    T = (imageTranslated-image)/alphaY
+    return imageTranslated.reshape(784), T.reshape(784)
     
 def TangenteDistance(p,e,Tp,Te):
     """
@@ -64,27 +69,17 @@ def TangenteDistance(p,e,Tp,Te):
     Tp et Te ont la même dimension
     Sortie: d: la distance tangente de p et e
     """
-    A = np.zeros((Tp.shape[0],2*Tp.shape[1]))
+    A = np.ones((1,2*784))
     A[:,0:Tp.shape[1]] = -1*Tp[:,:]
     A[:,Tp.shape[1]:2*Tp.shape[1]] = Te[:,:] # A = (-Tp Te)
     Q,R = qr(A) # decomposition QR de A
-    print(Q,R)
     Q2 = Q[:,R.shape[0]-1:Q.shape[1]]
     b = p-e
     d = Q2.transpose()@b
     return np.linalg.norm(d)
-    
-    
-                        
-p = ldb.getData(5)
-Tp = np.array([[2]])
 
-e = ldb.getData(15)
-Te = np.array([[-2]])
-
-Training, Test = ldb.seperateData()
-translationDB(Training,2,translateX)
-
+Training, Test = ldb.seperateData()    
+translationDB(Training,1,translateX)
 #M = ldb.getData(10)
 #trX = translateX(M,5)
 #trY = translateY(M,5)
