7a05841387aca49a6779d0c6d81ad38ed441013a
Bijan <jhmj>
2019-04-02 16:27:39 +0200

    test valid

--- GenerateTransformedData.py
+++ GenerateTransformedData.py
@@ -73,8 +73,6 @@ def TangenteDistance(p, e, Tp, Te):
     A[:, 0:cp] = -1 * Tp[:, :]
     A[:,cp:ce+cp] = Te[:, :]
     Q, R = qr(A)
-    print(Q)
-    print(R)
     lr,cr=R.shape
     lq,cq=Q.shape
     Q2 = Q[:,cr:cq]
@@ -90,10 +88,13 @@ alpha = 4
 #ldb.resetDataBase("translateX.mat")
 derivs = ldb.getDerivationDB("translateX.mat")
 p = ldb.getData(15)
-tp = np.array([derivs[15]])
+tp = np.array([derivs[15]]).transpose()
 e = ldb.getData(20)
-te = np.array([derivs[20]])
+te = np.array([derivs[20]]).transpose()
 print(TangenteDistance(p,e,tp,te))
+print(np.linalg.norm(e-p))
+ldb.afficheChiffre(ldb.getData(15))
+ldb.afficheChiffre(ldb.getData(20))
 # M = ldb.getData(10)
 # trX = translateX(M,5)
 # trY = translateY(M,5)
